4163c6f412515dfd617e8797fd2bd5bdcdb6fb37
KanittaMac <kanittamac@KanittaICI.local>
2026-07-04 16:03:51 +0700

    fix(vehicle-api): include ProblemTypeCode, FaultPartyCode, CarCaseCode, and ClaimNumber in SELECT query to fix edit dropdown pre-population

--- app/api/vehicle/[registerNo]/route.ts
+++ app/api/vehicle/[registerNo]/route.ts
@@ -146,6 +146,7 @@ export async function GET(
           m.MaintenanceStartDate, m.MaintenanceFinishDate, m.MaintenanceReturnDate,
           m.IssueTitle, m.CarStatusCode, m.ServiceLocationCode,
           m.InsuranceCode, m.DriverName, m.RegisterNo, m.VinNo, m.RootCauseFound, m.FixAction,
+          m.ProblemTypeCode, m.FaultPartyCode, m.CarCaseCode, m.ClaimNumber,
           m.LastFollowUpDate, m.ParentMaintenanceItemID,
           m.CreateDate, m.UpdateDate, m.CreateUserID, m.UpdateUserID,
           ISNULL(NULLIF(uc.FirstName + ' ' + ISNULL(uc.LastName, ''), ''), uc.UserName) AS CreateUserName,
